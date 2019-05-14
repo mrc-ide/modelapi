5762d08c8a1bde50dc13a39b5d1892138812d861
Rich FitzJohn <rich.fitzjohn@gmail.com>
2019-05-14 08:28:46 +0100

    Start getting basic queue working

---
+++ R/queue.R
@@ -0,0 +1,75 @@
+## The bits here will pretty much all be repurposable for a general
+## support package
+
+model_queue_start <- function(root, workers = 2, name = "modelapi",
+                              global = TRUE) {
+  if (!global || is.null(cache$queue)) {
+    ctx <- context::context_load(context_init(root, name))
+
+    message("connecting to redis at ", redux::redis_config()$url)
+    con <- redux::hiredis()
+
+    message("Starting queue")
+    rrq <- rrq::rrq_controller(ctx, con)
+    if (workers > 0L) {
+      rrq::worker_spawn(rrq, workers)
+      reg.finalizer(rrq, function(e) {
+        message("Stopping workers")
+        rrq$worker_stop()
+      })
+    }
+
+    if (!global) {
+      return(rrq)
+    }
+
+    cache$queue <- rrq
+  }
+  invisible(cache$queue)
+}
+
+model_queue_submit <- function(data, queue = cache$queue) {
+  queue$enqueue_(quote(model_run(data)))
+}
+
+
+model_queue_status <- function(id, queue = cache$queue) {
+  status <- unname(queue$task_status(id))
+  done <- c("ERROR", "COMPLETE")
+  if (status %in% done) {
+    list(done = TRUE,
+         status = status,
+         success = status == "COMPLETE",
+         queue = NULL)
+  } else {
+    list(done = FALSE,
+         status = status,
+         success = NULL,
+         queue = queue$task_position(id))
+  }
+}
+
+
+model_queue_result <- function(id, queue = cache$queue) {
+  queue$task_result(id)
+}
+
+
+model_queue_remove <- function(id, queue = cache$queue) {
+  queue$task_delete(id)
+}
+
+
+## Not part of the api exposed functions, used in tests
+model_queue_stop <- function(queue = cache$queue) {
+  queue$destroy(delete = TRUE)
+}
+
+
+## Support for queue building
+context_init <- function(root, name = "modelapi") {
+  context::context_save(root,
+                        sources = character(0),
+                        packages = "modelapi",
+                        name = name)
+}
---
+++ tests/testthat/test-queue.R
@@ -0,0 +1,52 @@
+context("queue")
+
+test_that("basic queue operation", {
+  queue <- model_queue_start(tempfile(), workers = 1, global = FALSE)
+  on.exit(model_queue_stop(queue))
+  expect_null(cache$queue)
+
+  inputs <- validate_inputs("a,b\n1,2\n2,3\n3,4\n",
+                            list(a = 20, b = 3, time = 0.5, poll = 100))
+  id <- model_queue_submit(inputs, queue = queue)
+
+  s1 <- model_queue_status(id, queue = queue)
+  expect_true(s1$status %in% c("PENDING", "RUNNING"))
+  expect_null(s1$success)
+  expect_equal(s1$queue, 0L)
+  expect_false(s1$done)
+
+  queue$task_wait(id, timeout = 2)
+  s2 <- model_queue_status(id, queue = queue)
+  expect_equal(s2$status, "COMPLETE")
+  expect_true(s2$success)
+  expect_null(s2$queue, 0L)
+  expect_true(s2$done)
+})
+
+
+test_that("queue length", {
+  queue <- model_queue_start(tempfile(), workers = 0, global = FALSE)
+  inputs <- validate_inputs("a,b\n1,2\n2,3\n3,4\n",
+                            list(a = 20, b = 3, time = 0.5, poll = 100))
+  id1 <- model_queue_submit(inputs, queue = queue)
+  id2 <- model_queue_submit(inputs, queue = queue)
+  id3 <- model_queue_submit(inputs, queue = queue)
+
+  s1 <- model_queue_status(id1, queue = queue)
+  s2 <- model_queue_status(id2, queue = queue)
+  s3 <- model_queue_status(id3, queue = queue)
+
+  expect_equal(s1$status, "PENDING")
+  expect_equal(s2$status, "PENDING")
+  expect_equal(s3$status, "PENDING")
+
+  expect_equal(s1$queue, 1)
+  expect_equal(s2$queue, 2)
+  expect_equal(s3$queue, 3)
+
+  ## This is actually a bug: need to remove the task from
+  ## keys$queue_rrq though that is a little difficult without some
+  ## transactional tricks
+  model_queue_remove(id2, queue = queue)
+  expect_equal(model_queue_status(id3, queue = queue)$queue, 3)
+})
